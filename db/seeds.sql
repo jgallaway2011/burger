09fa636d801e8d0e37ee15a1d06345a5926df944
jgallaway2011 <jgallaway2011@gmail.com>
2018-08-30 01:27:33 -0500

    Adding basic file structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Jalepeno Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
