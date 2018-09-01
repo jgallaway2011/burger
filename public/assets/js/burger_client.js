4bb1c9a0c3f86dca756fee403228d7af439803df
jgallaway2011 <jgallaway2011@gmail.com>
2018-09-01 18:20:50 -0500

    Clean up code

--- public/assets/js/burger_client.js
+++ public/assets/js/burger_client.js
@@ -2,19 +2,16 @@
 $(function () {
 
   $("#submit").click(function () {
-
     var newBurger = {
       burger_name: $("#new-burger").val().trim(),
       devoured: 0
     };
-    console.log(newBurger);
 
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function () {
-        console.log("Created new burger");
         location.reload();
       }
     );
@@ -23,19 +20,15 @@ $(function () {
   $(".change-devour").on("click", function (event) {
     var id = $(this).data("id");
     var newDevour = $(this).data("newdevour");
-    console.log(id);
-    console.log(newDevour);
     var newDevourState = {
       devoured: newDevour
     };
-    console.log(newDevourState);
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newDevourState
     }).then(
       function () {
-        console.log("Changed sleep to ", newDevour);
         // Reload the page to get the updated list
         location.reload();
       }
